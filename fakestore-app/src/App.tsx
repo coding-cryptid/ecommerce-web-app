0f0538f444f93d221e3afc4e120cc798412098ab
Gianna Cosenza <giacosey28@gmail.com>
2026-07-14 10:41:05 -0400

    Added routes and imports to app

--- fakestore-app/src/App.tsx
+++ fakestore-app/src/App.tsx
@@ -1,9 +1,18 @@
 import Home from './pages/Home.tsx'
+import Cart from './pages/Cart.tsx'
+import NavBar from './components/NavBar.tsx'
+import { Routes, Route } from 'react-router-dom'
 
 function App() {
     return (
-        <Home />
-    )
+        <>
+        <NavBar />
+        <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/cart" element={<Cart />} />
+        </Routes>
+        </>
+    );
 }
 
 export default App
